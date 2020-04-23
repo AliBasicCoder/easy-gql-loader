fa0908dd938a6174dd3f873b0ab1f633e6d87055
alianmad <aliahmedreda34@gmail.com>
2020-04-23 16:07:21 +0200

    fixing issue

--- src/defaultClient.js
+++ src/defaultClient.js
@@ -1,11 +1,15 @@
 module.exports = (config) => {
   var graphqlFetch = require("graphql-fetch")(config.url);
-  var swServer = require("subscriptions-transport-ws").SubscriptionClient;
-  var swClient = new swServer(config.webSocketEndPoint, {
-    reconnect: true,
-  });
+  var swServer;
+  var swClient;
+  if (config.webSocketEndPoint) {
+    swServer = require("subscriptions-transport-ws").SubscriptionClient;
+    swClient = new swServer(config.webSocketEndPoint, {
+      reconnect: true,
+    });
+  }
   return (type, query, vars, opts) => {
-    if (type === "subscription") {
+    if (type === "subscription" && config.webSocketEndPoint) {
       return swClient.request({
         query: str,
         variables: queryVars,
